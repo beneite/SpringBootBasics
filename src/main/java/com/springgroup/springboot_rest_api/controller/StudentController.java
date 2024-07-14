956b3b02b2a25b77760ebead56aa68a1d49dd262
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2024-07-14 12:08:59 +0530

    adding RequestMapping

--- src/main/java/com/springgroup/springboot_rest_api/controller/StudentController.java
+++ src/main/java/com/springgroup/springboot_rest_api/controller/StudentController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("MyCustomApi")
 public class StudentController {
 
     // url: http://localhost:8080/getStudentDetails
