d89215bfb8d23dfde698e96edaa98b7ba26bfb36
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2024-07-21 21:12:51 +0530

    adding Spring Boot REST API Validation

--- src/main/java/com/springgroup/springboot_rest_api/dto/UserDto.java
+++ src/main/java/com/springgroup/springboot_rest_api/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.springgroup.springboot_rest_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,18 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+
+    // validate the firstName should not be null/empty
+    @NotEmpty(message = "firstName should not be null or empty")
     private String firstName;
+
+    // validate the lastName should not be null/empty
+    @NotEmpty(message = "lastName should not be null or empty")
     private String lastName;
+
+    // validate the lastName should not be null/empty
+    // validate email is having valid format
+    @NotEmpty(message = "email should not be null or empty")
+    @Email(message = "email should be Formatted")
     private String email;
 }
