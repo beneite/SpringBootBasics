ed8613781fcb9fbe6310beb8a59f1fdb75415113
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2024-10-23 20:38:15 +0530

    adding Swagger-ui for API documentation

--- src/main/java/com/springgroup/springboot_rest_api/SpringbootRestApiApplication.java
+++ src/main/java/com/springgroup/springboot_rest_api/SpringbootRestApiApplication.java
@@ -1,20 +1,39 @@
 package com.springgroup.springboot_rest_api;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Sprint boot REST API Simple Student Record Application",
+				description = "This application perform all the simple crud operations",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Ashish Mishra",
+						email = "im.ashish707@gmail.com"
+				)
+        ),
+		externalDocs = @ExternalDocumentation(
+				description = "This is an external documentation",
+				url = "http://localhost:8080/api/user/"
+		)
+)
 public class SpringbootRestApiApplication {
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/springgroup/springboot_rest_api/projectInfo.md
+++ src/main/java/com/springgroup/springboot_rest_api/projectInfo.md
@@ -4,6 +4,12 @@
 
 <img src="gitresources/projectArch.png" alt=""/>
 
+### Swagger:
+<hr></hr>
+
+![swagger-ui.png](gitresources%2Fswagger-ui.png)
+<hr style="border:2px solid cyan">
+
 ## Interview Questions:
 > 1. JPA vs DTO
 > 2. What is @bean
